d84dc019b79d95ee89e6711606844d5599519ceb
Elio Galindo Fonseca <eliogalindo92@yahoo.com>
2022-01-05 00:05:24 -0500

    Beginning from scratch.

---
+++ hand_tracking_modules/hand_tracking.py
@@ -0,0 +1,38 @@
+import cv2
+import mediapipe as mp
+
+
+class HandTracker:
+
+    def __init__(self, mode=False, max_hands=2, detection_confidence=0.5, track_confidence=0.5):
+        self.mode = mode
+        self.max_hands = max_hands
+        self.detection_confidence = detection_confidence
+        self.track_confidence = track_confidence
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands()
+        self.mp_draw = mp.solutions.drawing_utils
+
+    def find_hands(self, image, draw=True):
+        rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(rgb_image)
+
+        if self.results.multi_hand_landmarks:
+            for hand_landmarks in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mp_draw.draw_landmarks(image, hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
+        return image
+
+    def find_position(self, image, hands_number=0, draw=True):
+
+        landmark_list = []
+        if self.results.multi_hand_landmarks:
+            hand = self.results.multi_hand_landmarks[hands_number]
+            for identifier, landmark in enumerate(hand.landmark):
+                height, width, channel = image.shape
+                coordinate_x, coordinate_y = int(landmark.x * width), int(landmark.y * height)
+                landmark_list.append([identifier, coordinate_x, coordinate_y])
+                if draw:
+                    cv2.circle(image, (coordinate_x, coordinate_y), 10, (50, 0, 255), cv2.FILLED)
+
+        return landmark_list
---
+++ hand_tracking_modules/main.py
@@ -0,0 +1,28 @@
+import cv2
+import time
+import hand_tracking as ht
+
+
+def main():
+    current_time = 0
+    previous_time = 0
+    cap = cv2.VideoCapture(0)
+    detector = ht.HandTracker()
+
+    while True:
+        success, image = cap.read()
+        image = detector.find_hands(image)
+        landmark_list = detector.find_position(image)
+        if len(landmark_list) != 0:
+            print(landmark_list[0])
+
+        current_time = time.time()
+        fps = 1 / (current_time - previous_time)
+        previous_time = current_time
+        cv2.putText(image, f"FPS:{str(int(fps))}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (50, 0, 255), 1)
+        cv2.imshow("Image", image)
+        cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
